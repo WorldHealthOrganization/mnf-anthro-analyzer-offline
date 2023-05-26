2d41a5b857804b6cab4c288cbdd55d803b072577
Richard Kumapley <rkumapley@users.noreply.github.com>
2023-05-26 13:05:31 +0200

    Renaming Folders

--- Unzipped/Version 1.0.3/Software/static/find-all-dependencies.R
+++
@@ -1,5 +0,0 @@
-# This scripts looks for dependent packages to be included on the software page
-
-packages <- renv::dependencies()
-packages <- sort(setdiff(unique(packages$Package), c("tools", "stats", "rsconnect")))
-saveRDS(packages, "static/dependent-packages.rds", version = 2)
